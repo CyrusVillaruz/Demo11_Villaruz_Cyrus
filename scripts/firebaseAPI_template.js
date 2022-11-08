e12767ed4aa383c06e08108cdd87f52989c945c6
CyrusVillaruz <cyrusvillaruz1@gmail.com>
2022-11-08 10:56:49 -0800

    Update firebaseAPI_template.js

--- scripts/firebaseAPI_template.js
+++ scripts/firebaseAPI_template.js
@@ -2,12 +2,18 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyC6Fd56ZfZLBUSvcvQLHCXuQeyPYYq52LQ",
+
+    authDomain: "comp1800-26540.firebaseapp.com",
+  
+    projectId: "comp1800-26540",
+  
+    storageBucket: "comp1800-26540.appspot.com",
+  
+    messagingSenderId: "848674265478",
+  
+    appId: "1:848674265478:web:a8ee3519163df84280f407"
+  
 };
 
 //--------------------------------------------
